205f489c62fde297978594ab145e54be4e6a0553
dukaegbu <dukaegbu8@gmail.com>
2025-06-12 20:31:13 -0500

    updating groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(String imagename) {
     echo 'building docker image'
     withCredentials([
         usernamePassword(
@@ -7,8 +7,10 @@ def call() {
             passwordVariable: 'PASSWORD'
         )
     ]) {
-        sh 'docker build -t dukaegbu/dbase-repo:myapp-2.0 .'
+        // sh 'docker build -t dukaegbu/dbase-repo:myapp-2.0 .'
+        sh "docker build -t $imagename ."
         sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
-        sh 'docker push dukaegbu/dbase-repo:myapp-2.0'
+        // sh 'docker push dukaegbu/dbase-repo:myapp-2.0'
+        sh "docker push $imagename"
     }
 }
